38da7742388adf41ae0151beaf32c4132f555b82
Larissa Araujo <larissa.jvsa@gmail.com>
2022-01-28 20:40:01 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import App from '../App';
+
+// realizei este requisito acompanhando a mentoria de revisão do Rod (summer)
+
+describe('Teste o componente <NotFound.js />', () => {
+  beforeEach(() => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/doesntexist');
+  });
+
+  it('Teste se página contém um heading h2 com o texto Page requested NotFound 😭', () => {
+    const titleNotFound = screen.getByRole('heading', {
+      level: 2,
+      name: /page requested not found/i,
+    });
+    expect(titleNotFound).toBeInTheDocument();
+  });
+
+  it('Teste se página mostra a imagem', () => {
+    const imgNotFound = screen.getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+    expect(imgNotFound).toHaveAttribute(
+      'src',
+      'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+    );
+    expect(imgNotFound).toBeInTheDocument();
+  });
+});
